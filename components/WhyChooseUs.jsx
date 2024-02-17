76c0345aae44a24aef2d0db9ffecfd2105fd8eac
puru12 <purushottam.makura@gmail.com>
2024-02-17 15:46:25 +0545

    fix server side rendering

--- components/WhyChooseUs.jsx
+++ components/WhyChooseUs.jsx
@@ -7,7 +7,7 @@ const WhyChooseUs = ({ data }) => {
       <h3 className="text-[#7D64FF] font-bold tracking-wider text-3xl md:text-4xl  text-center mb-10">
         Why Choose Us
       </h3>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+      <div className="grid lg:mx-24 lg:grid-cols-3 gap-3 md:grid-cols-2 grid-cols-1">
         {data.map((item) => (
           <div className="space-y-3 mb-10" key={item.id}>
             <div className="relative w-[100px] h-[100px]">
--- pages/index.js
+++ pages/index.js
@@ -15,62 +15,46 @@ import Reviews from "@/components/Reviews";
 import GoodCompany from "@/components/GoodCompany";
 
 import { useRouter } from "next/router";
-import { appUrl } from "@/lib/url";
+import { appUrl, dashboardUrl } from "@/lib/url";
 
 export async function getStaticProps({ locale }) {
+  const res = await fetch(`${dashboardUrl}/other/home`);
+  const choose = await fetch(`${dashboardUrl}/choose-us`);
+  const reviews = await fetch(`${dashboardUrl}/reviews`);
+  const company = await fetch(`${dashboardUrl}/company-image`);
+
+  let CompanyImagesData = await company.json();
+  CompanyImagesData = CompanyImagesData.data;
+  let reviewsData = await reviews.json();
+  reviewsData = reviewsData.data;
+
+  const { data } = await choose.json();
+
+  const { page } = await res.json();
+
   return {
     props: {
       ...(await serverSideTranslations(locale, ["common"])),
+      myData: page,
+      chooseUsData: data,
+      reviewsData: reviewsData,
+      CompanyImagesData: CompanyImagesData,
     },
   };
 }
 
-const Home = () => {
-  const [isWindows, setIsWindows] = useState(false);
+const Home = ({ myData, chooseUsData, reviewsData, CompanyImagesData }) => {
   const toolsData = useToolsData();
-  const [myData, setData] = useState(null);
-  const [settingsArray, setSettingsArray] = useState(null);
-  const [chooseUsData, setChooseUsData] = useState(null);
-  const [reviewsData, setReviewsData] = useState(null);
-  const [CompanyImagesData, setCompanyImageData] = useState(null);
-
   const router = useRouter();
   const currentUrl = router.asPath;
 
-  const [isLoading, setLoading] = useState(true);
   const { t } = useTranslation();
-  useEffect(() => {
-    fetch(`/api/other/${"home"}`)
-      .then((res) => res.json())
-      .then((data) => {
-        const { page } = data;
-        setData(page);
-        setSettingsArray(page.Settings[0]);
-        setLoading(false);
-        setIsWindows(navigator.userAgent.includes("Windows"));
-      });
-    fetch(`/api/choose-us`)
-      .then((res) => res.json())
-      .then((data) => {
-        setChooseUsData(data.data);
-      });
-    fetch(`/api/reviews`)
-      .then((res) => res.json())
-      .then((data) => {
-        setReviewsData(data.data);
-      });
-    fetch(`/api/company-image`)
-      .then((res) => res.json())
-      .then((data) => {
-        setCompanyImageData(data.data);
-      });
-  }, []);
 
   return (
     <>
       <Head>
         {/* Anything you add here will be added to this page only */}
-        <title>{myData?.metaTitle}</title>
+        {/* <title>{myData?.metaTitle}</title>
         {myData && (
           <>
             <meta property="og:title" content={myData.ogTitle} />
@@ -79,7 +63,7 @@ const Home = () => {
             <meta property="og:image:alt" content={myData.ogImageAlt} />
           </>
         )}
-        <meta name="description" content={myData?.metaDescription} />
+        <meta name="description" content={myData?.metaDescription} /> */}
 
         <meta
           name="Keywords"
@@ -173,84 +157,52 @@ const Home = () => {
 
       <>
         <main>
-          <section
-            className={`hero mt-8 mb-8 lg:mt-20 lg:mb-32 ${
-              isWindows ? "lg:mx-[12rem]" : "lg:mx-[5rem]"
-            } mx-[1rem] md:mx-[3.8rem]`}
-          >
+          <section className={`hero mt-8 mb-8   mx-[1rem] md:mx-[3.8rem]`}>
             <div className=" space-y-7 lg:flex items-center justify-around">
               <div className="lg:space-y-7 space-y-5">
-                {isLoading ? (
-                  <div className="bg-slate-200 h-6  w-[12rem] lg:w-[25rem] animate-pulse"></div>
-                ) : (
-                  <h1 className="text-[#7D64FF] font-bold tracking-wider text-2xl md:text-4xl ">
-                    {settingsArray?.title}
-                  </h1>
-                )}
-                {isLoading ? (
-                  <div className="space-y-1">
-                    <div className="bg-slate-200 h-6  w-[12rem] lg:w-[25rem] animate-pulse" />
-                    <div className="bg-slate-200 h-6  w-[12rem] lg:w-[25rem] animate-pulse" />
-                  </div>
-                ) : (
-                  <p className="lg:w-[40rem] lg:mr-20 tracking-normal text-xl">
-                    {settingsArray?.description}
-                  </p>
-                )}
+                <h1 className="text-[#7D64FF] font-bold tracking-wider text-2xl md:text-4xl ">
+                  {myData.Settings[0]?.title}
+                </h1>
+
+                <p className="lg:w-[40rem] lg:mr-20 tracking-normal text-xl">
+                  {myData.Settings[0]?.description}
+                </p>
+
                 <div className="md:space-x-6 space-y-2">
-                  {isLoading ? (
-                    <div className="h-[30px] w-[20px] md:w-auto bg-slate-200 flex animate-pulse" />
-                  ) : (
-                    <Button asChild className="w-full md:w-auto " size="lg">
-                      <Link href={settingsArray?.buttonHref}>
-                        {settingsArray?.button}
-                      </Link>
-                    </Button>
-                  )}
-                  {isLoading ? (
-                    <div className="h-[30px] flex w-[20px] md:w-auto bg-slate-200 animate-pulse" />
-                  ) : (
-                    <Button
-                      variant="outline"
-                      asChild
-                      className="w-full md:w-auto "
-                    >
-                      <Link href={settingsArray?.buttonTwoHref}>
-                        {settingsArray?.buttonTwo}
-                      </Link>
-                    </Button>
-                  )}
+                  <Button asChild className="w-full md:w-auto " size="lg">
+                    <Link href={myData.Settings[0]?.buttonHref}>
+                      {myData.Settings[0]?.button}
+                    </Link>
+                  </Button>
+
+                  <Button
+                    variant="outline"
+                    asChild
+                    className="w-full md:w-auto "
+                  >
+                    <Link href={myData.Settings[0]?.buttonTwoHref}>
+                      {myData.Settings[0]?.buttonTwo}
+                    </Link>
+                  </Button>
                 </div>
               </div>
               <div>
-                {isLoading ? (
-                  <div className="w-[500px] h-[500px] bg-slate-200 animate-pulse"></div>
-                ) : (
-                  <Image
-                    src={settingsArray?.image}
-                    alt={settingsArray?.imageAlt}
-                    width={700}
-                    height={400}
-                  />
-                )}
+                <Image
+                  src={myData.Settings[0]?.image}
+                  alt={myData.Settings[0]?.imageAlt}
+                  width={400}
+                  height={200}
+                />
               </div>
             </div>
           </section>
           <header className="page_section header mb-0">
             {/* <h1 className="title">{t("common:page_header_title")}</h1> */}
-            {isLoading ? (
-              <h2 className="title bg-slate-200 h-6  w-[12rem] lg:w-[25rem] animate-pulse"></h2>
-            ) : (
-              <h2 className="title">{myData.title}</h2>
-            )}
+            <h2 className="title">{myData.title}</h2>
             {/* <p className="description">{t("common:page_header_text")}</p>  */}
-            {isLoading ? (
-              <p className="title bg-slate-200 h-6  w-[15rem] lg:w-[25rem] animate-pulse"></p>
-            ) : (
-              <p className="description">
-                {myData?.description && parse(myData.description)}
-              </p>
-            )}
+            <div className="description">
+              {myData?.description && parse(myData.description)}
+            </div>
           </header>
           <section className="page_section mt-0">
             <article className="container">
@@ -289,33 +241,24 @@ const Home = () => {
           {/* simple task start  */}
 
           <section
-            className={`hero mt-8 mb-8 lg:mt-20 lg:mb-32 ${
-              isWindows ? "lg:mx-[12rem]" : "lg:mx-[5rem]"
-            } mx-[1rem] md:mx-[3.8rem]`}
+            className={`hero mt-8 mb-8 lg:mt-20 lg:mb-32  mx-[1rem] md:mx-[3.8rem]`}
           >
             <div className="space-y-3 mb-10">
-              {isLoading ? (
-                <h3 className="w-[30px] h-[10px] bg-slate-200 animate-pulse font-bold tracking-wider text-3xl md:text-4xl text-center "></h3>
-              ) : (
-                <h3 className="text-[#7D64FF] font-bold tracking-wider text-3xl md:text-4xl text-center ">
-                  {settingsArray?.tasksTitle}
-                </h3>
-              )}
-              {isLoading ? (
-                <p className="tracking-normal text-xl text-center text-black/70 w-[50px] h-[10px] bg-slate-200  "></p>
-              ) : (
-                <p className="tracking-normal text-xl text-center text-black/70  ">
-                  {settingsArray?.tasksDescription}
-                </p>
-              )}
+              <h3 className="text-[#7D64FF] font-bold tracking-wider text-3xl md:text-4xl text-center ">
+                {myData.Settings[0]?.tasksTitle}
+              </h3>
+
+              <p className="tracking-normal text-xl text-center text-black/70  ">
+                {myData.Settings[0]?.tasksDescription}
+              </p>
             </div>
             <div className=" space-y-7 lg:flex items-center justify-around">
               <div className="lg:space-y-7 space-y-5">
                 <h3 className="text-[#7D64FF] font-bold tracking-wider text-xl md:text-3xl ">
-                  {settingsArray?.tasksSubTitle}
+                  {myData.Settings[0]?.tasksSubTitle}
                 </h3>
                 <p className="lg:w-[40rem] lg:mr-20 tracking-normal text-xl">
-                  {settingsArray?.tasksSubDescription}
+                  {myData.Settings[0]?.tasksSubDescription}
                 </p>
                 <div className="">
                   <Button
@@ -324,22 +267,20 @@ const Home = () => {
                     size="lg"
                     variant="outline"
                   >
-                    {settingsArray && (
-                      <Link href={settingsArray?.tasksButtonHref}>
-                        {settingsArray?.tasksButton}
+                    <Link href={myData.Settings[0]?.tasksButtonHref}>
+                      {myData.Settings[0]?.tasksButton}
 
-                        <ArrowRight className="ml-1 w-5 h-5" />
-                      </Link>
-                    )}
+                      <ArrowRight className="ml-1 w-5 h-5" />
+                    </Link>
                   </Button>
                 </div>
               </div>
               <div>
                 <Image
-                  src={settingsArray?.tasksImage}
-                  alt={settingsArray?.tasksImageAlt}
-                  width={700}
-                  height={400}
+                  src={myData.Settings[0]?.tasksImage}
+                  alt={myData.Settings[0]?.tasksImageAlt}
+                  width={400}
+                  height={300}
                 />
               </div>
             </div>
@@ -347,9 +288,7 @@ const Home = () => {
           {/* simple task end */}
           {/* why choose us start  */}
           <section
-            className={`hero mt-8 mb-8 lg:mt-20 lg:mb-32 ${
-              isWindows ? "lg:mx-[12rem]" : "lg:mx-[5rem]"
-            } mx-[1rem] md:mx-[3.8rem]`}
+            className={`hero mt-8 mb-8 lg:mt-20 lg:mb-32  mx-[1rem] md:mx-[3.8rem]`}
           >
             {chooseUsData && <WhyChooseUs data={chooseUsData} />}
           </section>
@@ -357,13 +296,11 @@ const Home = () => {
 
           {/* good company start  */}
           <section
-            className={`hero mt-8 mb-8 lg:mt-20 lg:mb-32 ${
-              isWindows ? "lg:mx-[12rem]" : "lg:mx-[5rem]"
-            } mx-[1rem] md:mx-[3.8rem]`}
+            className={`hero mt-8 mb-8 lg:mt-20 lg:mb-32  mx-[1rem] md:mx-[3.8rem]`}
           >
             <div>
               <h3 className="text-[#7D64FF] font-bold tracking-wider text-3xl md:text-4xl  text-center mb-10">
-                {myData && myData.Settings[0].comapnyTitle}{" "}
+                {myData.Settings[0].comapnyTitle}{" "}
               </h3>
               {CompanyImagesData && <GoodCompany data={CompanyImagesData} />}
             </div>
@@ -372,9 +309,7 @@ const Home = () => {
 
           {/* review start  */}
           <section
-            className={`hero mt-8 mb-8 lg:mt-20 lg:mb-32 ${
-              isWindows ? "lg:mx-[12rem]" : "lg:mx-[5rem]"
-            } mx-[1rem] md:mx-[3.8rem]`}
+            className={`hero mt-8 mb-8 lg:mt-20 lg:mb-32  mx-[1rem] md:mx-[3.8rem]`}
           >
             {reviewsData && <Reviews data={reviewsData} />}
           </section>
