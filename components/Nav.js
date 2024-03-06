27ebc07e3ccd557afbccba84f4ba84bf26a1564d
puru12 <purushottam.makura@gmail.com>
2024-03-06 09:15:46 +0545

    revert navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,14 @@
-import { useTranslation } from "next-i18next";
-import Image from "next/image";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { useRouter } from "next/router";
-import React, { useEffect, useRef, useState } from "react";
-import { ChevronDown, List, X } from "react-bootstrap-icons";
-import useToolsData from "../hooks/useToolsData";
-import styles from "../styles/MegaMenu.module.css";
 import LangModal from "./LangModal";
-import LanguageCountryFlag from "./LanguageCountryFlag";
 import ToolsList from "./ToolsList";
-import { NavDown, NavUp } from "./icons/Icon";
+import { useTranslation } from "next-i18next";
+import LanguageCountryFlag from "./LanguageCountryFlag";
+import styles from "../styles/MegaMenu.module.css";
+import { List, ChevronDown, X, ChevronUp } from "react-bootstrap-icons";
+import useToolsData from "../hooks/useToolsData";
+import Image from "next/image";
 
 const Nav = React.memo(function Nav() {
   const router = useRouter();
@@ -19,7 +17,6 @@ const Nav = React.memo(function Nav() {
   const [showModal, setShowModal] = useState(false);
   const [navToggleShow, setNavToggleShow] = useState(false);
   const navMenuRef = useRef(null);
-  const path = usePathname();
 
   const handleRouteChange = () => {
     //Route changes
@@ -54,7 +51,7 @@ const Nav = React.memo(function Nav() {
     <>
       <header className="relative">
         <div className="fixed top-0 w-full z-[999]  ">
-          <nav className={`${styles.navigation} shadow-md relative`}>
+          <nav className={`${styles.navigation}`}>
             <Link href="/" className={`${styles.logo}`}>
               {/* <h3>
                 <span style={{ color: "#7d64ff" }}>PDF</span>
@@ -71,26 +68,12 @@ const Nav = React.memo(function Nav() {
             </Link>
             <ul
               ref={navMenuRef}
-              className={`
-              uppercase
-              ${styles.nav_menu} ${navToggleShow ? "" : ""}
-              
-              `}
+              className={`${styles.nav_menu} ${
+                navToggleShow ? styles.active : ""
+              }`}
             >
-              <li
-                className={`  ${styles.nav_list} ${styles.remove_in_mobile} `}
-              >
-                <Link
-                  href="/"
-                  className={`${
-                    styles.nav_link
-                  }  hover:text-[#EE1B22] text-[16px] font-[600]:
-                  ${path == "/" ? "text-[#EE1B22] relative " : ""}
-                  `}
-                >
-                  {path == "/" && (
-                    <span className="bg-red-500 h-[4px] w-[89px] absolute top-12 ml-3 "></span>
-                  )}
+              <li className={`${styles.nav_list} ${styles.remove_in_mobile}`}>
+                <Link href="/" className={`${styles.nav_link}`}>
                   <span>{t("common:home_page")}</span>
                 </Link>
               </li>
@@ -98,16 +81,9 @@ const Nav = React.memo(function Nav() {
               <li className={`${styles.nav_list} ${styles.remove_in_mobile}`}>
                 <Link
                   href={toolsData["MergePDFTool"].href}
-                  className={`${
-                    styles.nav_link
-                  }  hover:text-[#EE1B22] text-[16px] font-[500]
-                  ${path == "/merge-pdf" ? "text-[#EE1B22] relative " : " "}
-                  `}
+                  className={`${styles.nav_link}`}
                   prefetch={false}
                 >
-                  {path == "/merge-pdf" && (
-                    <span className="bg-red-500 h-[4px] w-[110px] absolute top-12 ml-2  "></span>
-                  )}
                   <span>{toolsData["MergePDFTool"].title}</span>
                 </Link>
               </li>
@@ -115,17 +91,9 @@ const Nav = React.memo(function Nav() {
               <li className={`${styles.nav_list} ${styles.remove_in_mobile}`}>
                 <Link
                   href={toolsData["CompressPDFTool"].href}
-                  className={`${
-                    styles.nav_link
-                  }  hover:text-[#EE1B22] text-[16px] font-[500] ${
-                    path == "/compress-pdf" ? "text-[#EE1B22] relative " : ""
-                  }
-                  `}
+                  className={`${styles.nav_link}`}
                   prefetch={false}
                 >
-                  {path == "/compress-pdf" && (
-                    <span className="bg-red-500 h-[4px] w-[150px] absolute top-12 ml-5  "></span>
-                  )}
                   <span>{toolsData["CompressPDFTool"].title}</span>
                 </Link>
               </li>
@@ -136,22 +104,13 @@ const Nav = React.memo(function Nav() {
                 }`}
               >
                 <div
-                  className={`${styles.nav_link} ${styles.remove_in_mobile} hover:text-[#EE1B22] flex items-center justify-center gap-2 relative`}
+                  className={`${styles.nav_link} ${styles.remove_in_mobile}`}
                   onClick={() => {
                     setNavToggleShow(!navToggleShow);
                   }}
                 >
-                  <span className=" font-[500] text-[16px]">
-                    {t("common:all_pdf_tools")}
-                  </span>
-                  {navToggleShow ? (
-                    <>
-                      {NavUp}
-                      <span className="bg-red-500 h-[4px] w-[200px] absolute top-12 ml-5  "></span>
-                    </>
-                  ) : (
-                    NavDown
-                  )}
+                  <span>{t("common:all_pdf_tools")}</span>
+                  {navToggleShow ? <ChevronUp /> : <ChevronDown />}
                 </div>
                 <div className={`${styles.dropdown}`}>
                   <div className={`${styles.dropdown_inner}`}>
