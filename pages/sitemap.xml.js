4c052e5ae5ddeabd9bbf6f4b91fa042ab230cdec
puru12 <purushottam.makura@gmail.com>
2024-03-31 19:52:20 +0545

    added sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,3 @@
-import React from "react";
 import fs from "fs";
 import fetch from "node-fetch"; // Importing fetch for Node.js environment
 import { dashboardUrl } from "@/lib/url";
@@ -6,11 +5,10 @@ import { dashboardUrl } from "@/lib/url";
 const Sitemap = () => {};
 
 export const getServerSideProps = async ({ res }) => {
-  const baseUrl = {
-    // development: "http://localhost:3000",
-    development: "https://www.allpdfconverter.com",
-    production: "https://www.allpdfconverter.com",
-  }[process.env.NODE_ENV];
+  const baseUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://www.allpdfconverter.com";
 
   const response = await fetch(`${dashboardUrl}/blogs`);
   const data = await response.json();
@@ -32,13 +30,6 @@ export const getServerSideProps = async ({ res }) => {
       return `${baseUrl}/${staticPagePath.split(".")[0]}`;
     });
 
-  <url>
-    <loc>"sdmndsmnnm</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.9</priority>
-  </url>;
-
   const staticUrls = staticPages
     .map((url) => {
       return `
@@ -57,7 +48,6 @@ export const getServerSideProps = async ({ res }) => {
       return `
         <url>
           <loc>${baseUrl}/blog/${item.slug}</loc>
-          // <lastmod>${new Date(item.updated_at).toISOString()}</lastmod>
           <changefreq>monthly</changefreq>
           <priority>0.9</priority>
         </url>
@@ -66,19 +56,213 @@ export const getServerSideProps = async ({ res }) => {
     .join("");
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap">
-    <url>
-          <loc>${baseUrl}</loc>
-          <lastmod>${new Date().toISOString()}</lastmod>
-          <changefreq>monthly</changefreq>
-          <priority>1.0</priority>
-        </url>
-      ${staticUrls}
-      ${blogUrls}
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${baseUrl}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+      </url>
+     
+     
+      <url>
+<loc>https://www.allpdfconverter.com/about</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+
+<url>
+<loc>https://www.allpdfconverter.com/blogs</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/bmp-to-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/compress-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/contacts</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/excel-to-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/extract-pdf-pages</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/grayscale-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+
+<url>
+<loc>https://www.allpdfconverter.com/jpg-to-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/merge-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/organize-pdf-pages</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/pdf-to-bmp</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/pdf-to-jpg</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/pdf-to-png</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/pdf-to-pptx</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/pdf-to-tiff</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/pdf-to-txt</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/pdf-to-word</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/pdf-to-zip</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/png-to-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/pptx-to-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/privacy-policy</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/protect-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/remove-pdf-pages</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/repair-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/rotate-pdf-pages</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/terms-of-use</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/tiff-to-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/txt-to-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/unlock-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://www.allpdfconverter.com/word-to-pdf</loc>
+<lastmod>2024-03-27T10:42:19.808Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.9</priority>
+</url>
+${blogUrls}
     </urlset>
   `;
 
-  res.setHeader("Content-Type", "text/xml");
+  // Set the response headers properly
+  res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate"); // Cache for 24 hours
+  res.setHeader("Vary", "Accept-Encoding");
+
+  // Send the XML content as the response
   res.write(sitemap);
   res.end();
 
