1b07729fc35b20a7cb1ac91edf883aba5f37b7dd
puru12 <purushottam.makura@gmail.com>
2024-04-01 11:18:20 +0545

    fixed sitemap as per seo

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -36,7 +36,7 @@ export const getServerSideProps = async ({ res }) => {
         <url>
           <loc>${url}</loc>
           <lastmod>${new Date().toISOString()}</lastmod>
-          <changefreq>monthly</changefreq>
+          <changefreq>daily</changefreq>
           <priority>0.9</priority>
         </url>
       `;
@@ -48,7 +48,8 @@ export const getServerSideProps = async ({ res }) => {
       return `
         <url>
           <loc>${baseUrl}/blog/${item.slug}</loc>
-          <changefreq>monthly</changefreq>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <changefreq>daily</changefreq>
           <priority>0.9</priority>
         </url>
       `;
@@ -56,201 +57,201 @@ export const getServerSideProps = async ({ res }) => {
     .join("");
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap">
       <url>
         <loc>${baseUrl}</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>monthly</changefreq>
+        <changefreq>daily</changefreq>
         <priority>1.0</priority>
       </url>
      
      
       <url>
 <loc>https://www.allpdfconverter.com/about</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 
 <url>
 <loc>https://www.allpdfconverter.com/blogs</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/bmp-to-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/compress-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/contacts</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/excel-to-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/extract-pdf-pages</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/grayscale-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 
 <url>
 <loc>https://www.allpdfconverter.com/jpg-to-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/merge-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/organize-pdf-pages</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/pdf-to-bmp</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/pdf-to-jpg</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/pdf-to-png</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/pdf-to-pptx</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/pdf-to-tiff</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/pdf-to-txt</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/pdf-to-word</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/pdf-to-zip</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/png-to-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/pptx-to-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/privacy-policy</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/protect-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/remove-pdf-pages</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/repair-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/rotate-pdf-pages</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/terms-of-use</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/tiff-to-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/txt-to-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/unlock-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 <url>
 <loc>https://www.allpdfconverter.com/word-to-pdf</loc>
-<lastmod>2024-03-27T10:42:19.808Z</lastmod>
-<changefreq>monthly</changefreq>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
 <priority>0.9</priority>
 </url>
 ${blogUrls}
